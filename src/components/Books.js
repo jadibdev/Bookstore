33f1dcf0fb6883a86bda0d5f52102dc57aedaff0
Mohamed Jadib <jadibdev@gmail.com>
2022-07-14 22:17:43 +0100

    Remove commented code, add MIT license to package.json

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,31 +1,3 @@
-/* import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-// import Book from './Book';
-import BookInput from './BookInput';
-import { remove } from '../redux/books/books';
-
-const Books = () => {
-  const books = useSelector((state) => state.books);
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      <ul>
-        {books.map((value) => (
-          <li key={value.id}>
-            <h2>{value.title}</h2>
-            <p>{value.author}</p>
-            <button type="button" onClick={() => { dispatch(remove(value.id)); }}>Remove</button>
-          </li>
-        ))}
-      </ul>
-      <BookInput />
-    </div>
-  );
-};
-
-export default Books; */
-
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchBooks } from '../redux/books/books';
